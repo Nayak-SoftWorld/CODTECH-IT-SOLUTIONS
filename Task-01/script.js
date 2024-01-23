9d67b1e13a07467aaeaba0462247cd478e76cd65
Manish Nayak <nayak10softworld@gmail.com>
2024-01-23 19:39:44 +0530

    nothing1
    nothing1

--- Task-01/script.js
+++ Task-01/script.js
@@ -5,7 +5,7 @@ let navbar = document.querySelector(".navbar");
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
-};
+}; 
 
 //=============== scroll section active link ============//
 let sections = document.querySelectorAll("section");
